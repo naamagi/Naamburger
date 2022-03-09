fa72e854aa245289cf90b18c851bbc831f721185
naamagi <n.gilat@gmail.com>
2022-03-09 02:58:07 +0200

    test

--- site.js
+++ site.js
@@ -1,5 +1,10 @@
 
+const express=require("express");const app=express();
+app.get("/",function(req,res){
+  res.send("working!");
+});
 
+app.listen(process.env.PORT||5000);
 
 let totalPrice = +10;
 
